11795cdbbebf560ceb1ad018b9ed08e074aac05b
Priyansh Singh <priyanshsingh855@gmail.com>
2026-01-12 23:56:15 +0530

    Create Directed Graph Cycle.py

---
+++ Directed Graph Cycle.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isCyclic(self, V, edges):
+        adj = [[] for _ in range(V)]
+        for u, v in edges:
+            adj[u].append(v)
+             
+        vis=[False]*V
+        path=[False]*V
+        
+        def dfs(node):
+            vis[node]=True
+            path[node]=True
+            
+            for neigh in adj[node]:
+                if not vis[neigh]:
+                    if dfs(neigh):
+                        return True
+                    
+                elif path[neigh]:
+                    return True
+                
+                
+            path[node]=False
+            
+            return False
+            
+            
+            
+        for i in range(V):
+            if not vis[i]:
+                if dfs(i):
+                    return True
+                    
+        return False
